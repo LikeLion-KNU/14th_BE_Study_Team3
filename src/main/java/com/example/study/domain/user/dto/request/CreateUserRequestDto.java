5c3560c31dccf2c5d0729a40c1c5852dab123ed5
baeeol <qata163753@gmail.com>
2026-04-09 13:13:08 +0900

    fix: 유저 name 필드를 request body로 받을 때 검증 로직 수정

--- src/main/java/com/example/study/domain/user/dto/request/CreateUserRequestDto.java
+++ src/main/java/com/example/study/domain/user/dto/request/CreateUserRequestDto.java
@@ -1,10 +1,12 @@
 package com.example.study.domain.user.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record CreateUserRequestDto(
     @NotNull(message = "이름은 필수입니다.")
-    @Size(min=1, max=12)
+    @NotBlank(message = "이름은 최소 1글자입니다.")
+    @Size(min=1, max=12, message = "이름은 최소 1글자, 최대 12글자입니다.")
     String name
 ) {}
--- src/main/java/com/example/study/domain/user/dto/request/ModifyUserRequestDto.java
+++ src/main/java/com/example/study/domain/user/dto/request/ModifyUserRequestDto.java
@@ -1,8 +1,10 @@
 package com.example.study.domain.user.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record ModifyUserRequestDto(
-   @Size(min=1, max=12)
+   @NotBlank(message = "이름은 최소 1글자입니다.")
+   @Size(min=1, max=12, message = "이름은 최소 1글자, 최대 12글자입니다.")
    String changedName
 ) {}
