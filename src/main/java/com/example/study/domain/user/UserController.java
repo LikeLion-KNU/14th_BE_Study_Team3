9ad01e7924c274c761d8a92430c66ed8565be250
baeeol <qata163753@gmail.com>
2026-04-09 20:52:19 +0900

    fix: UserController, @Controller -> @RestController

--- src/main/java/com/example/study/domain/user/UserController.java
+++ src/main/java/com/example/study/domain/user/UserController.java
@@ -2,7 +2,6 @@ package com.example.study.domain.user;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -20,9 +19,10 @@ import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class UserController {
     private UserService userService;
     
