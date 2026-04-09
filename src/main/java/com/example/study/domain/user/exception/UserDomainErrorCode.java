47002d1ef1383e5859ac402cff55eb8a5a4b988c
baeeol <qata163753@gmail.com>
2026-04-09 21:31:19 +0900

    fix: NOT_FOUND_USER Error Code의 status를 404 Not Found로변경

--- src/main/java/com/example/study/domain/user/exception/UserDomainErrorCode.java
+++ src/main/java/com/example/study/domain/user/exception/UserDomainErrorCode.java
@@ -12,7 +12,7 @@ import lombok.RequiredArgsConstructor;
 public enum UserDomainErrorCode implements ErrorCode {
     
     // user domain error code
-    NOT_FOUND_USER(HttpStatus.BAD_REQUEST, "NOT_FOUND_USER", "해당하는 아이디의 유저가 존재하지 않습니다."),
+    NOT_FOUND_USER(HttpStatus.NOT_FOUND, "NOT_FOUND_USER", "해당하는 아이디의 유저가 존재하지 않습니다."),
     NOT_VALID_REQUEST_BODY(HttpStatus.BAD_REQUEST, "NOT_VALID_REQUEST_BODY", "유효하지 않은 요청 바디입니다.");
 
     private final HttpStatus status;
