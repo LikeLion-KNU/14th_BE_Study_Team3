03335bb033d6f8e052dea55d78174bdcc7459d1c
baeeol <qata163753@gmail.com>
2026-04-29 00:15:05 +0900

    feat: user repository batch insert 구현

---
+++ src/main/java/com/example/study/domain/user/repository/UserJdbcRepository.java
@@ -0,0 +1,28 @@
+package com.example.study.domain.user.repository;
+
+import java.sql.PreparedStatement;
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.example.study.domain.user.User;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class UserJdbcRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public void batchInsert(List<User> userList) {
+        String sql = "INSERT INTO user (name) " + "VALUES (?)";
+
+        jdbcTemplate.batchUpdate(sql, 
+            userList, 
+            userList.size(), 
+            (PreparedStatement ps, User user) -> {
+                ps.setString(1, user.getName());
+            });
+    }
+}
