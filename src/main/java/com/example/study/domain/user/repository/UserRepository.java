9a814f78f8c738a7ea8dcbdd363952467adb7b4a
baeeol <qata163753@gmail.com>
2026-04-29 00:13:38 +0900

    rename: repository 클래스 폴더화

--- src/main/java/com/example/study/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/study/domain/user/repository/UserRepository.java
@@ -1,5 +1,7 @@
-package com.example.study.domain.user;
+package com.example.study.domain.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.study.domain.user.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {}
