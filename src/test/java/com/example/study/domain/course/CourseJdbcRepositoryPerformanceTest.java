5fbbc745a598b075c5828b615cf645a72755da9c
baeeol <qata163753@gmail.com>
2026-04-29 21:18:51 +0900

    chore: 불필요한 import 삭제

--- src/test/java/com/example/study/domain/course/CourseJdbcRepositoryPerformanceTest.java
+++ src/test/java/com/example/study/domain/course/CourseJdbcRepositoryPerformanceTest.java
@@ -4,14 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInfo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
-import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.util.StopWatch;
 
 import com.example.study.domain.course.entity.Course;
